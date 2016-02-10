9fff057cb96d3de2b6c546b4972125d36bf4fd1b
Joshua Gustafson and Matt Rosanio <silversevenfox@yahoo.com>
2016-02-10 08:37:32 -0800

    Add test for scissors beats paper

--- src/test/java/RockPaperScissorsTest.java
+++ src/test/java/RockPaperScissorsTest.java
@@ -5,8 +5,14 @@ import java.util.ArrayList;
 
 public class RockPaperScissorsTest {
   @Test
-public void checkWinner_rockBeatsScissors_true() {
-  RockPaperScissors testRockPaperScissors = new RockPaperScissors();
-  assertEquals(true, testRockPaperScissors.checkWinner("Rock", "Scissors"));
+  public void checkWinner_rockBeatsScissors_true() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    assertEquals(true, testRockPaperScissors.checkWinner("Rock", "Scissors"));
+  }
+
+  @Test
+  public void checkWinner_scissorsBeatsPaper_true() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    assertEquals(true, testRockPaperScissors.checkWinner("Scissors", "Paper"));
   }
 }
