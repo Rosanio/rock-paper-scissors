53d60209409f7b15921bab70db785e3a13427d9e
Joshua Gustafson and Matt Rosanio <silversevenfox@yahoo.com>
2016-02-10 11:07:15 -0800

    setup templates, add functionality to switch between webpages

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -8,29 +8,48 @@ import static spark.Spark.*;
 
 public class RockPaperScissors {
   public static void main(String[] args) {
-    
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+
+      model.put("template", "templates/play-game.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/result", (request, response) -> {
+      HashMap model = new HashMap();
+      String playerOne = request.queryParams("playerOne");
+      String playerTwo = request.queryParams("playerTwo");
+      String result = gameStatus(playerOne, playerTwo);
+
+      model.put("gameResult", result);
+
+      model.put("template", "templates/results.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
   public static Boolean checkWinner(String playerOneChoice, String playerTwoChoice) {
-    if (playerOneChoice == "Rock" && playerTwoChoice == "Scissors") {
+    if (playerOneChoice.equals("Rock") && playerTwoChoice.equals("Scissors")) {
       return true;
     }
-    if (playerOneChoice == "Scissors" && playerTwoChoice == "Paper") {
+    if (playerOneChoice.equals("Scissors") && playerTwoChoice.equals("Paper")) {
       return true;
     }
-    if (playerOneChoice == "Paper" && playerTwoChoice == "Rock") {
+    if (playerOneChoice.equals("Paper") && playerTwoChoice.equals("Rock")) {
       return true;
     }
     return false;
   }
 
   public static String gameStatus(String playerOneChoice, String playerTwoChoice) {
-    if(playerOneChoice == "random") {
+    if(playerOneChoice.equals("random")) {
       playerOneChoice = randomChoice();
     }
-    if(playerTwoChoice == "random") {
+    if(playerTwoChoice.equals("random")) {
       playerTwoChoice = randomChoice();
     }
-    if(playerOneChoice == playerTwoChoice){
+    if(playerOneChoice.equals(playerTwoChoice)){
       return "Everyone is a Winner!";
     }
     if (checkWinner(playerOneChoice, playerTwoChoice)) {
